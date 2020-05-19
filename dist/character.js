23be2b32321413f8b49e62e1f0a0708908d47ce7
three4c <three4c@gmail.com>
2020-05-20 01:27:22 +0900

    build

---
+++ dist/character.js
@@ -0,0 +1,9 @@
+import mongoose from "mongoose";
+mongoose.Promise = global.Promise;
+const CharacterSchema = new mongoose.Schema({
+    name: String,
+    age: Number,
+});
+const Character = mongoose.model("Character", CharacterSchema);
+export default Character;
+//# sourceMappingURL=character.js.map
--- dist/server.js
+++ dist/server.js
@@ -1,6 +1,93 @@
-"use strict";
-const hoge = (name) => {
-    return `Hello, ${name}!`;
-};
-console.log(hoge("TypeScript"));
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import Character from "./character";
+const app = express();
+const port = process.env.PORT || 3001;
+const dbUrl = "mongodb://localhost/crud";
+app.use(express.static("client/build"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+mongoose.connect(dbUrl, { useNewUrlParser: true, useUnifiedTopology: true }, (dbError) => {
+    if (dbError) {
+        console.log(dbError);
+        throw new Error(`${dbError}`);
+    }
+    else {
+        console.log("db connected");
+    }
+    app.get("/api/characters", (_, response) => {
+        Character.find({}, (error, characterArray) => {
+            if (error) {
+                response.status(500).send();
+            }
+            else {
+                response.status(200).send(characterArray);
+            }
+        });
+    });
+    app.post("/api/characters", (request, response) => {
+        const { name, age } = request.body;
+        if (!name && !age) {
+            return;
+        }
+        new Character({
+            name,
+            age,
+        }).save((error) => {
+            if (error) {
+                response.status(500).send();
+            }
+            else {
+                Character.find({}, (findError, characterArray) => {
+                    if (findError) {
+                        response.status(500).send();
+                    }
+                    else {
+                        response.status(200).send(characterArray);
+                    }
+                });
+            }
+        });
+    });
+    app.put("/api/characters", (request, response) => {
+        const { id } = request.body;
+        Character.findByIdAndUpdate(id, { $inc: { age: 1 } }, (error) => {
+            if (error) {
+                response.status(500).send();
+            }
+            else {
+                Character.find({}, (findError, characterArray) => {
+                    if (findError) {
+                        response.status(500).send();
+                    }
+                    else {
+                        response.status(200).send(characterArray);
+                    }
+                });
+            }
+        });
+    });
+    app.delete("/api/characters", (request, response) => {
+        const { id } = request.body;
+        Character.findByIdAndRemove(id, (error) => {
+            if (error) {
+                response.status(500).send();
+            }
+            else {
+                Character.find({}, (findError, characterArray) => {
+                    if (findError) {
+                        response.status(500).send();
+                    }
+                    else {
+                        response.status(200).send(characterArray);
+                    }
+                });
+            }
+        });
+    });
+    app.listen(port, () => {
+        console.log(`listening on port ${port}`);
+    });
+});
 //# sourceMappingURL=server.js.map
