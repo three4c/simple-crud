046996f01b8cc4a8ef0925a7a99cb4c1a35b98a3
three4c <three4c@gmail.com>
2020-05-24 14:56:51 +0900

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,17 @@
-const path = require("path");
-const nodeExternals = require("webpack-node-externals");
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+const dotenv = require('dotenv-webpack');
 
 module.exports = {
-  entry: "./src/server.ts",
-  target: "node", // Module not found: Error: Can't resolve 'fs' 対策
+  entry: './src/server.ts',
+  target: 'node', // Module not found: Error: Can't resolve 'fs' 対策
   externals: [nodeExternals()],
-  devtool: "inline-source-map",
+  devtool: 'inline-source-map',
   module: {
     rules: [
       {
-        enforce: "pre",
-        loader: "tslint-loader",
+        enforce: 'pre',
+        loader: 'tslint-loader',
         test: /\.ts$/,
         exclude: [/node_modules/],
         options: {
@@ -18,20 +19,21 @@ module.exports = {
         },
       },
       {
-        loader: "ts-loader",
+        loader: 'ts-loader',
         test: /\.ts$/,
         exclude: [/node_modules/],
         options: {
-          configFile: "tsconfig.json",
+          configFile: 'tsconfig.json',
         },
       },
     ],
   },
   resolve: {
-    extensions: [".ts", ".js"],
+    extensions: ['.ts', '.js'],
   },
   output: {
-    filename: "server.js",
-    path: path.resolve(__dirname, "dist"),
+    filename: 'server.js',
+    path: path.resolve(__dirname, 'dist'),
   },
+  plugins: [new dotenv({ systemvars: true })],
 };
